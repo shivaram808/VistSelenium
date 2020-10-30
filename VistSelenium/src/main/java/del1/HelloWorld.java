0fec759cb04986eb25087e9353dec2c991e4bde5
shiva ram <shivaram.marella@gmail.com>
2020-10-30 22:01:30 +0530

    Hello world program

---
+++ VistSelenium/src/main/java/del1/HelloWorld.java
@@ -0,0 +1,16 @@
+package del1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.test();
+	}
+	
+	public void test()
+	{
+		System.out.println("Hello World !!");
+	}
+
+}
