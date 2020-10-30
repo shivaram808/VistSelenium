4c14a0009c443a961aae360597d7523ab795824f
shiva ram <shivaram.marella@gmail.com>
2020-10-30 22:18:07 +0530

    Sample program

---
+++ VistSelenium/src/main/java/del1/SampleProg.java
@@ -0,0 +1,16 @@
+package del1;
+
+public class SampleProg {
+
+	public static void main(String[] args) {
+		
+		SampleProg sampleProg = new SampleProg();
+		sampleProg.test();
+	}
+	
+	public void test()
+	{
+		System.out.println("Sample !!");
+	}
+
+}
