f3f36e17ba1072148e747a88b80d2f687c26b9b9
sravanmuthu <59481894+sravanmuthu@users.noreply.github.com>
2020-10-31 13:49:34 +0530

    Sravan - Sample program

---
+++ VistSelenium/src/main/java/del1/SravanProg.java
@@ -0,0 +1,16 @@
+package del1;
+
+public class SravanProg {
+
+	public static void main(String[] args) {
+
+		SravanProg sravanProg = new SravanProg();
+		sravanProg.test();
+	}
+	
+	public void test()
+	{
+		System.out.println("Hello World !!");
+	}
+
+}
